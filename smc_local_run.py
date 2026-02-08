f40886a5662f97cc6a5268032ac98bc12fec2aea
Sudhir Voleti <sudhir_voleti@isb.edu>
2026-02-08 19:03:48 +0530

    Update smc_local_run.py

--- smc_local_run.py
+++ smc_local_run.py
@@ -83,6 +83,7 @@ if __name__ == "__main__":
     parser.add_argument("--state_specific_p", action="store_true")
     parser.add_argument("--n_cust", type=int, default=500)
     parser.add_argument("--draws", type=int, default=500)
+    parser.add_argument("--dataset", choices=['uci', 'cdnow'], default='uci')
     
     args = parser.parse_args()
     
